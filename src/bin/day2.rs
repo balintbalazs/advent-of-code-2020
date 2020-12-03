85ed1281023f7161f6275db3938264fda387453d
Balint Balazs <32197316+balintbalazs@users.noreply.github.com>
2020-12-04 00:31:37 +0100

    day3

--- src/bin/day2.rs
+++ src/bin/day2.rs
@@ -1,32 +1,23 @@
-use scan_fmt::scan_fmt;
-use std::fs::File;
-use std::io::{self, BufRead};
-use std::path::Path;
+use std::error::Error;
 
-fn main() {
+use advent_of_code_2020::read_lines;
+use scan_fmt::scan_fmt;
+
+fn main() -> Result<(), Box<dyn Error>> {
     let mut valid = (0, 0);
-    if let Ok(lines) = read_lines("day2_in.txt") {
-        for line in lines {
-            if let Ok(line) = line {
-                if check_rule1(&line) {
-                    valid.0 = valid.0 + 1;
-                }
-                if check_rule2(&line) {
-                    valid.1 = valid.1 + 1;
-                }
+    for line in read_lines("day2_in.txt")? {
+        if let Ok(line) = line {
+            if check_rule1(&line) {
+                valid.0 = valid.0 + 1;
+            }
+            if check_rule2(&line) {
+                valid.1 = valid.1 + 1;
             }
         }
     }
     println!("{} passwords are valid with rule 1", valid.0);
     println!("{} passwords are valid with rule 2", valid.1);
-}
-
-// The output is wrapped in a Result to allow matching on errors
-// Returns an Iterator to the Reader of the lines of the file.
-fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
-where P: AsRef<Path>, {
-    let file = File::open(filename)?;
-    Ok(io::BufReader::new(file).lines())
+    Ok(())
 }
 
 fn check_rule1(input: &str) -> bool {
@@ -44,7 +35,7 @@ fn check_rule2(input: &str) -> bool {
     let (i, j, c, s) = scan_fmt!(input, "{d}-{d} {}: {}", usize, usize, char, String).unwrap();
     let c = c as u8;
     let s = s.as_bytes();
-    (s[i-1] == c) ^ (s[j-1] == c)
+    (s[i - 1] == c) ^ (s[j - 1] == c)
 }
 
 #[test]
---
+++ src/bin/day3.rs
@@ -0,0 +1,25 @@
+use std::error::Error;
+
+use advent_of_code_2020::read_lines;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let slopes = vec![(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)];
+    let width = 31;
+    let mut line_number = 0;
+    let mut trees = [0; 5];
+    for line in read_lines("day3_in.txt")? {
+        let line = line?;
+        let line = line.as_bytes();
+        for i in 0..5 {
+            if line_number % slopes[i].1 == 0 {
+                if line[(slopes[i].0 * line_number / slopes[i].1) % width] == '#' as u8 {
+                    trees[i] = trees[i] + 1;
+                }
+            }
+        }
+        line_number = line_number + 1;
+    }
+    println!("Found {:#?} trees", trees);
+    println!("Product is {}", trees.iter().product::<i64>());
+    Ok(())
+}
